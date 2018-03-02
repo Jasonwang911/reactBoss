dd52e4294b88ce1aa793fe2c3835448f90a7d5ce
jasonwang911 <396689144@qq.com>
2018-03-02 22:41:28 +0800

    添加拦截器

--- src/config.js
+++ src/config.js
@@ -4,13 +4,13 @@ import {
 	Toast
 } from 'antd-mobile';
 
-// 拦截器
+// 请求拦截器
 axios.interceptors.request.use(function(config) {
 	Toast.loading('加载中', 0)
 	return config;
 })
 
-// 
+// 响应拦截器
 axios.interceptors.request.use(function(config) {
 	setTimeout(() => {
 		Toast.hide();
